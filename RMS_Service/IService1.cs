9782351d18544b88d2835de4a538cf0ad8c17d7e
Devang Patel <68782286+devangpatelmks@users.noreply.github.com>
2021-02-21 23:31:48 +0530

    Add files via upload

---
+++ RMS_Service/IService1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+
+namespace RMS_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string Insert(InsertOrder order);
+
+        [OperationContract]
+        getdata GetOI();
+
+        [OperationContract]
+        string Update(UpdateOI o);
+
+        [OperationContract]
+        string Delete(DeleteOI d);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class DeleteOI {
+        int o_id;
+
+        [DataMember]
+        public int O_ID {
+            get { return o_id; }
+            set { o_id = value; }
+        }
+
+    }
+
+    [DataContract]
+    public class UpdateOI {
+        int o_id;
+        string o_name;
+        string o_desc;
+        string o_price;
+        string o_type;
+        string o_stat;
+        string o_cate;
+
+        [DataMember]
+        public int O_ID {
+            get { return o_id; }
+            set { o_id = value; }
+        }
+
+        [DataMember]
+        public string O_Name
+        {
+            get { return o_name; }
+            set { o_name = value; }
+        }
+
+        [DataMember]
+        public string O_Desc
+        {
+            get { return o_desc; }
+            set { o_desc = value; }
+        }
+
+        [DataMember]
+        public string O_Price
+        {
+            get { return o_price; }
+            set { o_price = value; }
+        }
+
+        [DataMember]
+        public string O_Type
+        {
+            get { return o_type; }
+            set { o_type = value; }
+        }
+
+        [DataMember]
+        public string O_Stat
+        {
+            get { return o_stat; }
+            set { o_stat = value; }
+        }
+
+        [DataMember]
+        public string O_Cate
+        {
+            get { return o_cate; }
+            set { o_cate = value; }
+        }
+    }
+
+
+
+    [DataContract]
+    public class getdata {
+        [DataMember]
+        public DataTable Res_Tab {
+            get;
+            set;
+        }
+    }
+    
+    
+    [DataContract]
+    public class InsertOrder
+    {
+        string o_name = string.Empty;
+        string o_desc = string.Empty;
+        string o_price = string.Empty;
+        string o_type = string.Empty;
+        string o_stat = string.Empty;
+        string o_cate = string.Empty;
+
+        [DataMember]
+        public string O_Name
+        {
+            get { return o_name; }
+            set { o_name = value; }
+        }
+
+        [DataMember]
+        public string O_Desc
+        {
+            get { return o_desc; }
+            set { o_desc = value; }
+        }
+
+        [DataMember]
+        public string O_Price
+        {
+            get { return o_price; }
+            set { o_price = value; }
+        }
+
+        [DataMember]
+        public string O_Type
+        {
+            get { return o_type; }
+            set { o_type = value; }
+        }
+
+        [DataMember]
+        public string O_Stat
+        {
+            get { return o_stat; }
+            set { o_stat = value; }
+        }
+
+        [DataMember]
+        public string O_Cate
+        {
+            get { return o_cate; }
+            set { o_cate = value; }
+        }
+    }
+}
--- RMS_Service/Service1.cs
+++ RMS_Service/Service1.cs
@@ -1,1 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
 
+namespace RMS_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        public string Insert(InsertOrder order)
+        {
+            string msg;
+            SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Res_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into Res_Tab (O_Name, O_Desc, O_Price, O_Type, O_Stat, O_Cate) values (@O_Name, @O_Desc, @O_Price, @O_Type, @O_Stat, @O_Cate)", con);
+            cmd.Parameters.AddWithValue("@O_Name", order.O_Name ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@O_Desc", order.O_Desc ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@O_Price", order.O_Price ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@O_Type", order.O_Type ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@O_Stat", order.O_Stat ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@O_Cate", order.O_Cate ?? (object)DBNull.Value);
+
+            int g = cmd.ExecuteNonQuery();
+            if (g == 1)
+            {
+                msg = "Successfully inserted.";
+            }
+            else
+            {
+                msg = "Failed to insert.";
+            }
+
+            return msg;
+        }
+
+        public  getdata GetOI() {
+            getdata g = new getdata();
+            SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Res_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Res_Tab", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable("mytab");
+            da.Fill(dt);
+            g.Res_Tab = dt;
+            return g;
+        }
+
+        public string Update(UpdateOI o) {
+            string Message = "";
+            SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Res_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update Res_Tab set O_Name = @O_Name, O_Desc = @O_Desc, O_Price = @O_Price, O_Type = @O_Type, O_Stat = @O_Stat, O_Cate = @O_Cate WHERE O_ID = @O_ID", con);
+            cmd.Parameters.AddWithValue("@O_ID", o.O_ID);
+            cmd.Parameters.AddWithValue("@O_Name", o.O_Name ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@O_Desc", o.O_Desc ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@O_Price", o.O_Price ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@O_Type", o.O_Type ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@O_Stat", o.O_Stat ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@O_Cate", o.O_Cate ?? (object)DBNull.Value);
+            int res = cmd.ExecuteNonQuery();
+            if (res == 1)
+            {
+                Message = "Successfully updated.";
+            }
+            else
+            {
+                Message = "Failed to update.";
+            }
+
+            return Message;
+        }
+
+        public string Delete(DeleteOI d) {
+            string msg = "";
+            SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Res_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete Res_Tab where O_ID = @O_ID", con);
+            cmd.Parameters.AddWithValue("@O_ID", d.O_ID);
+            int res = cmd.ExecuteNonQuery();
+            if (res == 1)
+            {
+                msg = "Deleted Successfully.";
+                
+            }
+            else
+            {
+                msg = "Failed to delete.";
+            }
+
+            return msg;
+
+        }
+
+    }
+}
